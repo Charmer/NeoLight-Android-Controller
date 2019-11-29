ae2a54f3570b9031264ded4c9510fecf8c744fd5
Тимур <ahm2006@yandex.ru>
2019-11-30 01:54:06 +0300

    Отображение в списке устройств только с именем NeoLight* Улучшено визуальное отображение списка устройств для соединения Исправлен баг с отправкой команды группе устройств

--- app/src/main/java/ru/charmlab/neolight/WebAppInterface.java
+++ app/src/main/java/ru/charmlab/neolight/WebAppInterface.java
@@ -2,9 +2,11 @@ package ru.charmlab.neolight;
 
 import android.content.Context;
 import android.os.ParcelUuid;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 import android.bluetooth.*;
+
 import org.json.*;
 
 import java.io.IOException;
@@ -18,7 +20,7 @@ import java.util.Set;
 
 public class WebAppInterface {
     private Map<String, BluetoothDevice> devices = new HashMap<String, BluetoothDevice>();
-    private Map<String,BluetoothSocket> sockets = new HashMap<String, BluetoothSocket>();
+    private Map<String, BluetoothSocket> sockets = new HashMap<String, BluetoothSocket>();
     private Context mContext;
 
     /**
@@ -37,37 +39,42 @@ public class WebAppInterface {
     }
 
     @JavascriptInterface
-    public String searchDecice() throws org.json.JSONException, IOException{
+    public String searchDecice() throws org.json.JSONException, IOException {
         BluetoothAdapter bluetooth = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = bluetooth.getBondedDevices();
+        String DeviceList = "";
         this.devices.clear();
         this.sockets.clear();
-        JSONObject obj = new JSONObject();
+
         if (pairedDevices.size() > 0) {
+            DeviceList = "[";
             for (BluetoothDevice device : pairedDevices) {
-                if(device.getName().equals("HC-05")){
+                if (device.getName().length() > 8 && device.getName().substring(0,8).equals("NeoLight")) {
                     devices.put(device.getAddress(), device);
-                    obj.put("address", device.getAddress());
+                    DeviceList += "{\"address\":\"" + device.getAddress() + "\", \"name\":\"" + device.getName() + "\"},";
                     BluetoothSocket socket = device.createRfcommSocketToServiceRecord(device.getUuids()[0].getUuid());
                     socket.connect();
                     this.sockets.put(device.getAddress(), socket);
                 }
             }
+            DeviceList = DeviceList.substring(0, DeviceList.length() - 1);
+            DeviceList += "]";
         }
-        return obj.toString();
+        return DeviceList;
     }
 
     @JavascriptInterface
-    public void  SendColor(String color) throws org.json.JSONException, IOException
-    {
+    public void SendColor(String color) throws org.json.JSONException, IOException {
+        Log.i("SendColor", color);
         JSONObject jsonObject = new JSONObject(color.trim());
         BluetoothSocket socket;
         Iterator<String> keys = jsonObject.keys();
-        InputStream tmpIn=null;
-        OutputStream tmpOut=null;
-        while(keys.hasNext()) {
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+        while (keys.hasNext()) {
             String key = keys.next();
             String value = jsonObject.get(key).toString();
+            Log.i("SendColor", key);
             BluetoothDevice device = this.devices.get(key);
             ParcelUuid[] uuids = device.getUuids();
             socket = this.sockets.get(device.getAddress());
